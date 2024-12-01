fa88b4e3269e36e34eafbee40b4c4005e1863bfb
VNAT <xepjk@protonmail.com>
2024-12-01 19:14:22 +0100

    Point free programming should not be taken to the point where it is unreadable

--- day1/part1.hs
+++ day1/part1.hs
@@ -6,7 +6,8 @@ import System.Environment (getArgs)
 -- Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
 
 sumOfDistances :: (Ord a, Num a) => [a] -> [a] -> a
-sumOfDistances l r = sum $ zipWith (curry $ abs . uncurry (-)) (sort r) (sort l)
+sumOfDistances l r = sum $ zipWith absDiff (sort r) (sort l)
+  where absDiff a b = abs (a - b)
 
 main :: IO ()
 main = do

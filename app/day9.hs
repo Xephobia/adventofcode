4af625fe77794e0e1764e48e15c70d03158e286a
VNAT <xepjk@protonmail.com>
2024-12-09 22:19:46 +0100

    Add day 9 part 2

--- app/day9.hs
+++ app/day9.hs
@@ -1,6 +1,6 @@
 import Data.Char (isDigit)
 import Data.Maybe (fromMaybe, isJust, isNothing)
-import Data.Sequence (Seq)
+import Data.Sequence (Seq ((:<|), (:|>)), (|>))
 import qualified Data.Sequence as Seq
 import System.Environment (getArgs)
 
@@ -8,6 +8,11 @@ diskLayout :: [(Int, Int)] -> Seq (Maybe Int)
 diskLayout m = Seq.fromList $ concatMap file $ zipWith (\sf i -> (i, sf)) m [0 ..]
   where
     file (i, (s, f)) = replicate s (Just i) ++ replicate f Nothing
+
+diskLayout' :: [(Int, Int)] -> Seq (Seq (Maybe Int))
+diskLayout' m = Seq.fromList $ concatMap file $ zipWith (\sf i -> (i, sf)) m [0 ..]
+  where
+    file (i, (s, f)) = if f == 0 then [Seq.replicate s (Just i)] else [Seq.replicate s (Just i), Seq.replicate f Nothing]
 
 compactFiles :: Seq (Maybe Int) -> Seq (Maybe Int)
 compactFiles fs = cfs fs
@@ -25,6 +30,24 @@ compactFiles fs = cfs fs
       where
         (ffu, r) = Seq.spanl isJust fs'
         (ffr, r') = Seq.spanl isNothing r
+
+forcesIn :: Seq a -> Seq a -> Seq (Seq a)
+forcesIn a b = if Seq.null droped then sa else sa |> droped
+  where
+    droped = Seq.drop (Seq.length a) b
+    sa = Seq.singleton a
+
+compactFiles' :: Seq (Seq (Maybe Int)) -> Seq (Maybe Int)
+compactFiles' fs = foldl1 (<>) $ cfs fs
+  where
+    cfs :: Seq (Seq (Maybe Int)) -> Seq (Seq (Maybe Int))
+    cfs Seq.Empty = Seq.empty
+    cfs (e :<| Seq.Empty) = Seq.singleton e
+    cfs (l :|> rs@(r :<| _)) = case r of
+      Nothing -> cfs l :|> rs
+      Just _ -> case Seq.spanl (\es@(e :<| _) -> not (isNothing e && Seq.length es >= Seq.length rs)) l of
+        (fu, fr :<| frs) -> cfs (fu <> forcesIn rs fr <> frs |> Seq.replicate (Seq.length rs) Nothing)
+        (fu, Seq.Empty) -> cfs l |> rs
 
 checksum :: Seq (Maybe Int) -> Int
 checksum s = sum $ Seq.mapWithIndex (flip ((*) . fromMaybe 0)) s
@@ -44,3 +67,4 @@ main = do
   (inputPath : _) <- getArgs
   input <- getPuzzleInput inputPath
   print $ checksum $ compactFiles $ diskLayout input
+  print $ checksum $ compactFiles' $ diskLayout' input

35929d2dcd9be8d09cdb8cba1b6ffafd65a4b3e2
VNAT <xepjk@protonmail.com>
2024-12-15 16:26:26 +0100

    Add day 14 part 2

--- app/day14.hs
+++ app/day14.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 import Control.Lens.Each (each)
 import Control.Lens.Fold (foldl1Of)
-import Control.Monad.Trans.State.Strict (State, evalState, get, put)
-import Data.Char
+import Control.Monad (zipWithM_)
+import Control.Monad.Trans.State.Strict (State, evalState, get, put, runState)
+import Data.Char (isDigit)
 import Data.Foldable (foldl')
-import Data.Ix
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HSet
+import Data.Hashable (Hashable)
+import GHC.Generics (Generic)
 import System.Environment (getArgs)
 import Text.ParserCombinators.ReadP (ReadP, char, eof, munch1, option, optional, readP_to_S, sepBy1, string)
 
-newtype Pt = Pt (Int, Int) deriving (Ord, Eq, Show)
+newtype Pt = Pt (Int, Int) deriving (Ord, Eq, Show, Generic)
+
+instance Hashable Pt
 
 instance Num Pt where
   (Pt (a1, b1)) + (Pt (a2, b2)) = Pt (a1 + a2, b1 + b2)
@@ -16,11 +24,6 @@ instance Num Pt where
   signum (Pt (a, b)) = Pt (signum a, signum b)
   fromInteger i = Pt (fromInteger i, fromInteger i)
   negate (Pt (a, b)) = Pt (-a, -b)
-
-instance Ix Pt where
-  range (Pt (lx, ly), Pt (ux, uy)) = [Pt (x, y) | y <- range (ly, uy), x <- range (lx, ux)]
-  index (Pt (lx, ly), Pt (ux, uy)) (Pt (ix, iy)) = index (ly, uy) iy * rangeSize (lx, ux) + index (lx, ux) ix
-  inRange (Pt b1, Pt b2) (Pt c) = inRange (b1, b2) c
 
 type Robot = (Pt, Pt)
 
@@ -61,12 +64,23 @@ getPuzzleInput p = do
   raw <- readFile p
   return $ head [x | (x, _) <- readP_to_S (robots <* optional (char '\n') <* eof) raw]
 
+showPts :: HashSet Pt -> String
+showPts sp = replicate dimX '=' ++ '\n' : concatMap (\y -> map (\x -> if HSet.member (Pt (x, y)) sp then '#' else ' ') [0 .. dimX - 1] ++ ['\n']) [0 .. dimY - 1] ++ '\n' : replicate dimX '='
+
+values :: Int -> State a b -> a -> [b]
+values 0 _ _ = []
+values n st si = a : values (n - 1) st s
+  where
+    (a, s) = runState st si
+
 main :: IO ()
 main = do
   (ip : _) <- getArgs
   i <- getPuzzleInput ip
   let rs = evalState (foldl1 (>>) $ replicate 100 walk) i
   print $ foldl1Of each (*) $ foldl' count (0, 0, 0, 0) rs
+  let a = values 60000 walk i
+  zipWithM_ (\n s -> putStrLn $ show n ++ showPts (HSet.fromList s)) [1 ..] a -- search "#####################" in the output, you'll find the tree along with the number at the top of the "frame"
   where
     btwn l r a = l <= a && a <= r
     count (n1, n2, n3, n4) (Pt (x, y))
